39a805f411e1ea4c67f6b4cea39f4bafceb98592
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-09-14 13:38:38 +0300

    Task crud

--- Gemfile
+++ Gemfile
@@ -7,20 +7,21 @@ gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 #############################
 gem 'pg', '~> 0.18'
 
-
 #############################
-##  Server gems
-#############################
-gem 'puma', '~> 3.0'
-
-#############################
-### Frontend
+### Frontend gems
 #############################
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.5'
+gem 'hamlit'
+
+
+#############################
+##  Backend gems
+#############################
+gem 'puma', '~> 3.0'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy, :share]
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
-  def kek
+  def share
     respond_to :js
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:text)
+    end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
