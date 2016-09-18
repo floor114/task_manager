d13d7d5516c90a43fe1621fc3ca26793a26799cc
Taras Shpachenko <taras.shpachenko@yandex.ru>
2016-09-19 01:18:19 +0300

    refactor

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,10 +5,10 @@ class Task < ApplicationRecord
   validates :text, presence: true, length: { maximum: 150 }
 
   def creator
-    users_tasks.where(user_type: 0).first.user
+    users_tasks.find_by_user_type(:creator).user
   end
 
   def share(user_id)
-    users_tasks.create(user: User.find(user_id), user_type: 1)
+    users_tasks.create(user: User.find(user_id), user_type: :allowed)
   end
 end
